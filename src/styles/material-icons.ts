542a7b0a31291f2c5dfb1a4a31f5d76c7d1bc3cc
naukkarinene <einari.naukkarinen@outlook.com>
2020-07-28 17:09:27 +0100

    feat: add service worker

--- src/styles/material-icons.ts
+++
@@ -1,9 +0,0 @@
-import { css } from '@polymer/lit-element';
-
-export default css`
-  .material-icons {
-    font-family: 'Material Icons';
-    font-style: normal;
-    font-weight: 400;
-  }
-`;
---
+++ workbox-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  globDirectory: 'dist',
+  globPatterns: ['**/*.{html,js,css,png,svg,jpg,gif,json,woff,woff2,eot,ico,webmanifest,map,ttf}'],
+  swDest: 'dist/service-worker.js',
+  clientsClaim: true,
+  skipWaiting: true
+};
